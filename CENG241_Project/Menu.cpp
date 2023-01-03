cc0631d36d5f4e0a500074d0d8d7a41ce6e72466
Mustafa Kemal Öz <ozmustafakemal@gmail.com>
2023-01-03 14:24:42 +0300

    a

--- CENG241_Project/Menu.cpp
+++ CENG241_Project/Menu.cpp
@@ -39,10 +39,83 @@ void Menu::Start()		//Function output character selection menu
 			}
 				
 			break;
+		case 1:
+			pos.X = 4;
+			pos.Y = 4;
+			drawArr(Player(1).art, Player(1).artsize, pos);
+			cout << endl;
+			cout << "Character name:" << Player(1).name << endl;
+			cout << "Character HP:" << Player(1).getMaxHP() << endl;
+			cout << "Character AP:" << Player(1).getattackPow() << endl;
+			cout << "Do you confirm character?(y/n)";
+			cin >> characterConfirm;
+			if (characterConfirm == "y") {
+				characterChoice = tempChoice;
+			}
+			else if (characterConfirm == "n") {
+				system("cls");
+				goto point;
+
+			}
+			else {
+				cout << "Wrong input!" << endl;
+				goto point;
+			}
+
+			break;
+		case 2:
+			pos.X = 4;
+			pos.Y = 4;
+			drawArr(Player(2).art, Player(2).artsize, pos);
+			cout << endl;
+			cout << "Character name:" << Player(2).name << endl;
+			cout << "Character HP:" << Player(2).getMaxHP() << endl;
+			cout << "Character AP:" << Player(2).getattackPow() << endl;
+			cout << "Do you confirm character?(y/n)";
+			cin >> characterConfirm;
+			if (characterConfirm == "y") {
+				characterChoice = tempChoice;
+			}
+			else if (characterConfirm == "n") {
+				system("cls");
+				goto point;
+
+			}
+			else {
+				cout << "Wrong input!" << endl;
+				goto point;
+			}
+
+			break;
+		case 3:
+			pos.X = 4;
+			pos.Y = 4;
+			drawArr(Player(3).art, Player(3).artsize, pos);
+			cout << endl;
+			cout << "Character name:" << Player(3).name << endl;
+			cout << "Character HP:" << Player(3).getMaxHP() << endl;
+			cout << "Character AP:" << Player(3).getattackPow() << endl;
+			cout << "Do you confirm character?(y/n)";
+			cin >> characterConfirm;
+			if (characterConfirm == "y") {
+				characterChoice = tempChoice;
+			}
+			else if (characterConfirm == "n") {
+				system("cls");
+				goto point;
+
+			}
+			else {
+				cout << "Wrong input!" << endl;
+				goto point;
+			}
+
+			break;
 
 		default:
 			cout << "Wrong input!" << endl;
 			goto point;
+			
 			break;
 	}
 	
